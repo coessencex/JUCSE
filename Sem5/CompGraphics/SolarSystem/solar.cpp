738cd3eaf02ce6034ce33cd92dd40fdf2f52a764
Aishik Pyne <aishikpyne@gmail.com>
2017-11-08 17:35:56 +0530

    Sun added

--- Sem5/CompGraphics/SolarSystem/solar.cpp
+++ Sem5/CompGraphics/SolarSystem/solar.cpp
@@ -11,8 +11,8 @@ const string planet_names[8] = {"MERCURY", "VENUS", "EARTH", "MARS", "JUPITER", 
 // const double planet_a[MAX_PLANETS] = {57.9, 108.2, 149.6, 227.9, 778.6, 1433.5, 2872.5, 4495.1};
 // const double planet_b[MAX_PLANETS] = {50, 100, 130, 200, 700, 1300, 2550, 4000};
 const double planet_a[MAX_PLANETS] = {40, 75, 100, 140, 200, 240, 280, 310};
-const double planet_b[MAX_PLANETS] = {50, 100, 130, 200, 700, 1300, 2550, 4000};
-const double planet_speed[MAX_PLANETS] = {4.78, 3.50, 2.97, 2.40, 1.30, 0.96, 0.68, -0.54};
+const double planet_b[MAX_PLANETS] = {25, 55, 70, 100, 155, 190, 230, 210};
+const double planet_speed[MAX_PLANETS] = {4.78, -3.50, 2.97, 2.40, 1.30, 0.96, -0.68,0.54};
 const double planet_theta[MAX_PLANETS] = {0 ,0 ,0 ,0 ,0 ,0 ,0 ,0};
 const double planet_radius[MAX_PLANETS] = {4.8, 12.1, 12.7, 6.7, 142.9, 120.5, 52.118, 49.5};
 const int planet_color[MAX_PLANETS] = {LIGHTGRAY, YELLOW, BLUE, RED, LIGHTRED, CYAN, LIGHTCYAN, LIGHTBLUE};
@@ -45,17 +45,7 @@ class Planet {
         this->py = sin(this->theta)*(this->a*this->b)/sqrt(pow(this->b*cos(this->theta), 2)+pow(this->a*sin(this->theta), 2));
 
     }
-    void setdata(string name, double a, double b, double speed, double theta =0,int radius=100, int color=WHITE){
-        this->name = name;
-        this->a = a;
-        this->b = b;
-        this->speed = speed*pi/180.0;
-        this->radius = radius;
-        this->color = color;
-        this->theta = theta;
-        this->px = cos(this->theta)*(this->a*this->b)/sqrt(pow(this->b*cos(this->theta), 2)+pow(this->a*sin(this->theta), 2));
-        this->py = sin(this->theta)*(this->a*this->b)/sqrt(pow(this->b*cos(this->theta), 2)+pow(this->a*sin(this->theta), 2));
-    }
+
     void drawPath(){
         setcolor(this->color);
         ellipse(shiftx(0), shifty(0), 0, 360, this->a, this->b);
@@ -80,12 +70,13 @@ class SolarSystem{
 
     SolarSystem(){
         for(int i=0;i<MAX_PLANETS;i++){
-            this->planets[i] = new Planet(planet_names[i], planet_a[i], 0.6*planet_a[i], planet_speed[i], planet_theta[i], planet_radius[i]/11, planet_color[i]);
+            this->planets[i] = new Planet(planet_names[i], planet_a[i], planet_b[i], planet_speed[i], planet_theta[i], planet_radius[i]/8, planet_color[i]);
         }
     }
     void simulate(int iterations){
         while(iterations--){
-
+            setcolor(YELLOW);
+            fillellipse(shiftx(-10), shifty(0), 15, 15);
             for(int i=0;i<MAX_PLANETS;i++){
                 this->planets[i]->drawPath();
                 this->planets[i]->drawPlanet();
@@ -103,7 +94,7 @@ int main()
     int gd = DETECT, gm;
     initgraph(&gd, &gm, NULL);
     SolarSystem ss;
-    ss.simulate(400);
+    ss.simulate(500);
 
 
     delay(400000);
